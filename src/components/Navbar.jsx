61ec3968074e37188fa5e38309cf02315d33a1be
+ <+>
2024-07-26 14:02:53 +0300

    modify navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Link } from 'react-router-dom'
-import Home from '../pages/Home'
-import About from '../pages/About'
-import Services from '../pages/Services'
-import Tasks from '../pages/Tasks'
-import Contact from '../pages/Contact'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faBagShopping, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-      <nav className=' flex items-center justify-between bg-accent1 p-4'>
-        <Link to='' className='font-bold text-white text-lg'><FontAwesomeIcon icon={faHome} className='mr-5 text-3xl'/>Fountain Designs</Link>
-        <div className='flex space-x-6'>
-        <NavLink to='/' className='text-white nav-link' element={<Home/>}>Home</NavLink>
-        <NavLink to='/about' className='text-white nav-link' element={<About/>}>About Us</NavLink>
-        <NavLink to='/services' className='text-white nav-link' element={<Services/>}>Services</NavLink>
-        <NavLink to='/projects' className='text-white nav-link' element={<Tasks/>}>Projects</NavLink>
-        <NavLink to='/contact' className='text-white nav-link' element={<Contact/>}>Contact Us</NavLink>
+      <nav className='flex items-center justify-between bg-accent1 p-4'>
+        <Link to='/' className='font-bold text-white text-lg'>
+          <FontAwesomeIcon icon={faBagShopping} className='mr-5 text-3xl'/>
+          Fountain Designs
+        </Link>
+
+        <div className='md:hidden'>
+          <button 
+          onClick={toggleNav}
+          className='text-white p-2'
+          >
+            <FontAwesomeIcon icon={isOpen ? faTimes : faBars} className='text-2xl'/>
+          </button>
         </div>
-        
+
+        <div className={`flex-col md:flex md:flex-row md:space-x-6 md:items-center ${isOpen ? 'flex' : 'hidden'} md:flex`}>
+          <NavLink 
+            to='/' 
+            className='text-white nav-link' 
+            onClick={() => setIsOpen(false)}
+          >
+            Home
+          </NavLink>
+
+          <NavLink 
+            to='/about'
+            className='text-white nav-link' 
+            onClick={() => setIsOpen(false)} 
+          >
+            About Us
+          </NavLink>
+
+          <NavLink 
+            to='/services' 
+            className='text-white nav-link' 
+            onClick={() => setIsOpen(false)}
+          >
+            Services
+          </NavLink>
+
+          <NavLink 
+            to='/projects' 
+            className='text-white nav-link' 
+            onClick={() => setIsOpen(false)}
+          >
+            Projects
+          </NavLink>
+
+          <NavLink 
+            to='/contact' 
+            className='text-white nav-link' 
+            onClick={() => setIsOpen(false)}
+          >
+            Contact Us
+          </NavLink>
+        </div>
       </nav>
   )
 }
